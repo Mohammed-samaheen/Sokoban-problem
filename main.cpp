bf3752d23475b567b3a858fa6ae86a2a0cbfa233
Mohammed-samaheen <smai2011643@gmail.com>
2021-05-01 14:48:50 +0300

    end of save and load with test

--- main.cpp
+++ main.cpp
@@ -1,30 +1,56 @@
 #include <bits/stdc++.h>
+#include <fstream>
+#include "lip/boost/archive/text_oarchive.hpp"
+#include "lip/boost/archive/text_iarchive.hpp"
+#include "lip/boost/serialization/vector.hpp"
+#include "lip/boost/serialization/map.hpp"
 
 using namespace std;
 
 int const MAZE_SIZE=9;
 
-vector<vector<int> > maze {{' ',' ',' ','W','W','W','W','W',' '},
-                           {' ','W','W','W',' ',' ',' ','W',' '},
-                           {' ','W','G',' ','B',' ',' ','W',' '},
-                           {' ','W','W','W',' ','B','G','W',' '},
-                           {' ','W','G','W','W','B',' ','W',' '},
-                           {' ','W',' ','W',' ','G',' ','W','W'},
-                           {' ','W','B','B','G','B','B','G','W'},
-                           {' ','W',' ',' ',' ','G',' ',' ','W'},
-                           {' ','W','W','W','W','W','W','W','W'}};
+vector<vector<char>> maze {{' ', ' ', ' ', 'W', 'W', 'W', 'W', 'W', ' '},
+                           {' ', 'W', 'W', 'W', ' ', ' ', ' ', 'W', ' '},
+                           {' ', 'W', 'G', ' ', 'B', ' ', ' ', 'W', ' '},
+                           {' ', 'W', 'W', 'W', ' ', 'B', 'G', 'W', ' '},
+                           {' ', 'W', 'G', 'W', 'W', 'B', ' ', 'W', ' '},
+                           {' ', 'W', ' ', 'W', ' ', 'G', ' ', 'W', 'W'},
+                           {' ', 'W', 'B', 'B', 'G', 'B', 'B', 'G', 'W'},
+                           {' ', 'W', ' ', ' ', ' ', 'G', ' ', ' ', 'W'},
+                           {' ', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'}};
 vector<pair<int,int> > goalLocation;
 bool invalidPoint(int x,int y);
 vector<pair<int,int> > generateNextStates(int x,int y);
+void save( const string& filePath,map <vector<vector<char>>, map <char, int>> & data);
+map <vector<vector<char>>, map <char, int>> load(const string& filePath);
 void findGoalLocation();
 
 int main()
 {
-    findGoalLocation();
-
+    map <vector<vector<char>>, map <char, int>> m,n;
+    map <char, int> b;
+    b['c']=1;
+    m[maze]=b;
+    save("test",m);
+    n= load("test");
+    cout<<(n==m)<<endl;
     return 0;
 }
 
+void save( const string& filePath,map <vector<vector<char>>, map <char, int>> & data){
+    ofstream ofs(filePath);
+    boost::archive::text_oarchive oa(ofs);
+
+    oa<<data;
+}
+map <vector<vector<char>>, map <char, int>> load(const string& filePath){
+    map <vector<vector<char>>, map <char, int>> data;
+    ifstream ifs(filePath);
+    boost::archive::text_iarchive ia(ifs);
+    ia>>data;
+    return data;
+
+}
 // find location (x,y) of each goal point
 void findGoalLocation(){
     for (int i = 0; i < MAZE_SIZE; ++i)
@@ -33,6 +59,7 @@ void findGoalLocation(){
                 goalLocation.emplace_back(i,j);
 }
 
+// generate the valid next state
 vector<pair<int,int> > generateNextStates(int x,int y){
     vector<pair<int,int> > validState;
     int x_move[] ={0,1,-1,0};
@@ -48,7 +75,7 @@ vector<pair<int,int> > generateNextStates(int x,int y){
             int _x = x+x_move[i];
             int _y = y+y_move[i];
 
-            if(invalidPoint(_x,_y)||maze[_x][_y]=='B')
+            if(invalidPoint(_x,_y)||maze[_x][_y]=='B' )
                 continue;
         }
         validState.emplace_back(next_x,next_y);
@@ -58,7 +85,7 @@ vector<pair<int,int> > generateNextStates(int x,int y){
 }
 
 bool invalidPoint(int x,int y){
-    return x>=MAZE_SIZE || y>=MAZE_SIZE||maze[x][y]=='W';
+    return x>=MAZE_SIZE || y>=MAZE_SIZE||maze[x][y]=='W' || x<0 || y<0;
 }
 
 bool isEndState(int x,int y){
